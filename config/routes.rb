cc40d98cc7eb6b70d9bed52be18146a44e628a64
KatoArata <arata@Macintoshs-MacBook-Pro.local>
2021-05-08 10:44:24 +0700

    override resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do 
 	root 'pages#home'
 	devise_for :users, path:'', path_names: {sign_in: 'login', sign_out:'logout', sign_up: 'register'}
+
 	resources :profiles
+	get 'profiles/:id', to: 'profiles#show', as: 'profile_show'
+
 	get 'pages/home'
 	get 'about-me', to: 'pages#about'
 	get 'contact', to: 'pages#contact'
