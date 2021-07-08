0380252e7be43e2185ff91518e4311b9b953b07e
katoarata <katoarata1421997@gmail.com>
2021-07-08 18:51:22 +0700

    implemented refactor for navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,4 +20,39 @@ module ApplicationHelper
         DevcampViewTool::Renderer.copyright 'Jordan Hudgens', 'All rights reserved'
     end
 
+    def nav_items
+        [
+            {
+                url:root_path,
+                title:'home'
+            },
+            {
+                url:about_me_path,
+                title:'About Me'
+            },
+            {
+                url:contact_path,
+                title:'Contact'
+            },
+            {
+                url:blogs_path,
+                title:'Blog'
+            },
+            {
+                url:profiles_path,
+                title:'Portfolios'
+            },
+        ]
+    end
+
+    def nav_helper style, tag_type
+        nav_links=''
+        nav_items.each do |item|
+            nav_links<<"<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+        end
+        nav_links.html_safe
+    end
+    def active? path
+        "active" if current_page? path
+    end
 end
